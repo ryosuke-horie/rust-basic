bcb8b4446501f75b4dba5163a2eec0a7f608947c
ryosuke-horie <ryosuke.horie37@gmail.com>
2024-09-11 07:55:58 +0900

    match state

--- match/match.rs
+++ match/match.rs
@@ -1,8 +1,16 @@
+#[derive(Debug)]
+// アメリカの州を表すEnum
+enum UsState {
+    Alabama,
+    Alaska,
+    // ... などなど
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState), // Quater硬貨はアメリカの州のデザインを持つ
 }
 
 // コインをセントの価値に変換する
@@ -15,6 +23,11 @@ fn value_in_cents(coin: Coin) -> u8 {
         }
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25
+        // value_in_cents(Coin::Quarter(UsState::Alaska))で呼び出すと
+        // State quarter from Alaska! と表示され、25が返される
+        Coin::Quarter(state) =< {
+            println!("State quarter from {state:?}!"); // 
+            25
+        }
     }
 }
